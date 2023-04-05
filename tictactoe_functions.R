a419604d48203fa5e9fc673810b1e275073e0c60
Dawson Kinsman <60490532+dkinsman@users.noreply.github.com>
2023-04-05 15:59:53 -0400

    update function to play tictactoe

--- tictactoe_functions.R
+++ tictactoe_functions.R
@@ -1,45 +1,64 @@
-checkWin = function (){
+####################### Given board, check if there's a winner #################
+checkWin = function (board){
+  winner = NA
   
-  player1 = which(self$board==1)
-  player2 = which(self$board==-1)
-  cat('P1:', player1, '\nP2:', player2)
+  player1 = which(board==1)
+  player2 = which(board==-1)
+  #cat('P1:', player1, ' P2:', player2, '\n')
   # have to check columns, rows, and diagonals for a win,
   # we'll use a magic square, the indices should sum to 15!
-  if (length(player1) < 3 & length(player2) <3) return(self$winner)
-  
-  # UNCOMMENT AFTER DONE TESTING
+  if (length(player1) >= 3){
   p1_moves <- combinations(length(player1), 3, player1)
   for (i in 1:nrow(p1_moves)){
     if(sum(p1_moves[i,])==15){
-      self$winner = 1
-      return(self$winner)
+      winner = 1
+      return(winner)
     }
-  }
+  }}
   
+  if (length(player2) >=3){
   p2_moves <- combinations(length(player2), 3, player2)
   #print(p2_moves)
   for (i in 1:nrow(p2_moves)){
     if(sum(p2_moves[i,])==15){
-      self$winner = -1
-      return(self$winner)
+      winner = -1
+      return(winner)
     }
-  }
+  }}
   
   #declares a draw
-  if (length(which(is.na(self$board)))==0){
-    self$winner = 0
-    return(self$winner)
+  if (length(which(is.na(board)))==0){
+    winner = 0
+    return(winner)
   }
+  
+  return(winner)
 }
 
-randomPlay = function() {
+################################ MAKE A RANDOM PLAY ############################
+randomPlay = function(board, player) {
   # gets the indices of open boxes on the board
-  open_box = which(is.na(self$board$board))
+  open_box = which(is.na(board))
   # chooses a random box according to a uniform dist.
   move = sample(open_box,1)
   
   # player 1 is 1, player 2 is -1
-  self$board$board[move] = ifelse(self$player==1, 1, -1)
-  invisible(self)
+  board[move] = ifelse(player==1, 1, -1)
+  return(board)
 }
 
+########################### PLAY GAME ##########################################
+tictactoe = function(player){
+  board = rep(NA, 9)
+  while (is.na(checkWin(board))){
+    board = randomPlay(board, player)
+    player = -1 * player
+    
+    player1 = which(board==1)
+    player2 = which(board==-1)
+    cat('P1:', player1, ' P2:', player2, '\n')
+  }
+  return(checkWin(board))
+}
+
+tictactoe(1)
